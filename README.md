51443504cffc401eca55d1d6d2d5460a2d2491c4
luismendes070 <machadoluismendes@gmail.com>
2024-12-06 11:00:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# area-poligono
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/luismendes070/area-poligono)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Area do Polígono LeafletJS JS Starter</h1>`;
